f396979e37d8d2e3e7a2480e03fa0dfcd3e598fe
robinofverberg <91734293+robinofverberg@users.noreply.github.com>
2024-09-16 15:49:09 +0200

    Starting point

---
+++ BudgetingApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetingApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BudgetingApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BudgetingApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public ObservableCollection <int > Accounts { get; set; } = [1,2,3];
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
